d5bc99f5b9bfc0782852c8297f03f1c6a52f4f53
Jordan <kimminhyuk0719@gmail.com>
2021-02-26 15:02:52 +0900

    Temporary Test for api test

--- src/component/TopBar.js
+++ src/component/TopBar.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 function TopBar() {
 	return(
 		<div className="TopBar">
+			{/* <Link to="/"><image></image></Link> */}
 			<Link to="/">강의 리스트</Link>
 			<span> 맞춤 추천 받기</span>
 			<span> 고객센터</span>
